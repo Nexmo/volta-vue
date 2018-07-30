9ea29fd9cc1d3010e14bca82f55c34ea9883e282
kmanktelow <karen.manktelow@vonage.com>
2018-07-30 14:57:06 +0100

    add select component

---
+++ src/components/VltSelect.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="Vlt-select">
+    <select :id="id">
+      <option
+        v-for="o in options"
+        :value="o.value || o"
+        :key="o.value || o"
+      >
+        {{o.label || o}}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VltSelect',
+
+  props: {
+    id: String,
+    options: Array,
+  },
+};
+</script>
