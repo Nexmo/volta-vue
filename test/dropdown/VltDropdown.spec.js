5e06746d83e4739db4697fddf0d003ca2d7664a6
kmanktelow <karen.manktelow@vonage.com>
2018-12-17 15:02:31 +0000

    add dropdown tests

---
+++ test/dropdown/VltDropdown.spec.js
@@ -0,0 +1,76 @@
+import { shallowMount } from '@vue/test-utils';
+import { VltDropdown } from '../../src/components';
+
+describe('vlt-dropdown', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallowMount(VltDropdown);
+  });
+
+  test('is a Vue instance', () => {
+    expect(wrapper.isVueInstance()).toBeTruthy();
+  });
+
+  test('renders correctly', () => {
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  test('renders correctly with label "Pick one"', () => {
+    wrapper.setProps({
+      label: 'Pick one',
+    });
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  test('can open dropdown panel with basic options', () => {
+    wrapper.setProps({
+      options: ['red', 'yellow', 'blue'],
+    });
+    wrapper.find('button').trigger('click');
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  test('second option is selected and selection is shown', () => {
+    wrapper.setProps({
+      options: ['red', 'yellow', 'blue'],
+      showSelection: true,
+    });
+    wrapper.find('button').trigger('click');
+    wrapper.findAll('.Vlt-dropdown__link').at(1).trigger('click');
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  test('has label, second option is selected, selection not shown', () => {
+    wrapper.setProps({
+      options: ['red', 'yellow', 'blue'],
+      label: 'Pick one',
+    });
+    wrapper.find('button').trigger('click');
+    wrapper.findAll('.Vlt-dropdown__link').at(1).trigger('click');
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  test('has label, second option is selected, selection shown', () => {
+    wrapper.setProps({
+      options: ['red', 'yellow', 'blue'],
+      label: 'Pick one',
+      showSelection: true,
+      hideLabel: true,
+    });
+    wrapper.find('button').trigger('click');
+    wrapper.findAll('.Vlt-dropdown__link').at(1).trigger('click');
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  test('has label, second option is selected, selection and label shown', () => {
+    wrapper.setProps({
+      options: ['red', 'yellow', 'blue'],
+      label: 'Pick one',
+      showSelection: true,
+    });
+    wrapper.find('button').trigger('click');
+    wrapper.findAll('.Vlt-dropdown__link').at(1).trigger('click');
+    expect(wrapper.element).toMatchSnapshot();
+  });
+});
