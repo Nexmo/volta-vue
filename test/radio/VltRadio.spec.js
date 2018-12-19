1f6be78431a9aa10b796f456c52af40d16693072
kmanktelow <karen.manktelow@vonage.com>
2018-12-19 09:45:34 +0000

    radio button tests

---
+++ test/radio/VltRadio.spec.js
@@ -0,0 +1,29 @@
+import { shallowMount } from '@vue/test-utils';
+import { VltRadio } from '../../src/components';
+
+describe('vlt-radio', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallowMount(VltRadio, {
+      propsData: {
+        label: 'Do I pass?',
+        name: 'pass',
+        val: 'Yes',
+      },
+    });
+  });
+
+  test('is a Vue instance', () => {
+    expect(wrapper.isVueInstance()).toBeTruthy();
+  });
+
+  test('renders correctly with label "Do I pass?", name "pass" and value "Yes"', () => {
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  test('emits "yes" on value change', () => {
+    wrapper.find('input').trigger('click');
+    expect(wrapper.emitted('input')).toEqual([['Yes']]);
+  });
+});
