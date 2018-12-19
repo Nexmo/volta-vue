66a138a1c478205ca0e260ee2ffedaed731164bf
kmanktelow <karen.manktelow@vonage.com>
2018-12-19 11:04:44 +0000

    tests for vlt-select

---
+++ test/select/VltSelect.spec.js
@@ -0,0 +1,28 @@
+import { shallowMount } from '@vue/test-utils';
+import { VltSelect } from '../../src/components';
+
+describe('vlt-select', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallowMount(VltSelect, {
+      propsData: {
+        val: undefined,
+        options: ['red', 'yellow', 'blue'],
+      },
+    });
+  });
+
+  test('is a Vue instance', () => {
+    expect(wrapper.isVueInstance()).toBeTruthy();
+  });
+
+  test('renders correctly with options "red", "yellow", "blue"', () => {
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  test('selected value is "blue"', () => {
+    wrapper.findAll('option').at(2).setSelected();
+    expect(wrapper.emitted('input')).toEqual([['blue']]);
+  });
+});
