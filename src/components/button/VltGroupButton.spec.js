7b5acce7c3cd874dd3d1266429bbed20d5f9c06d
Grreg Frileux <greg.frileux@vonage.com>
2021-02-02 10:33:07 +0000

    fix button

--- src/components/button/VltGroupButton.spec.js
+++ src/components/button/VltGroupButton.spec.js
@@ -1,11 +1,11 @@
-import { shallowMount } from '@vue/test-utils';
+import { mount } from '@vue/test-utils';
 import { VltGroupButton } from '..';
 
 describe('vlt-group-button', () => {
   let wrapper;
 
   beforeEach(() => {
-    wrapper = shallowMount(VltGroupButton, {
+    wrapper = mount(VltGroupButton, {
       propsData: {
         options: ['red', 'yellow', 'blue'],
       },
@@ -16,21 +16,26 @@ describe('vlt-group-button', () => {
     expect(wrapper.element).toMatchSnapshot();
   });
 
-  test('is big', () => {
+  test('is big', async () => {
     wrapper.setProps({ big: true });
+    await wrapper.vm.$nextTick();
     expect(wrapper.element).toMatchSnapshot();
   });
 
-  test('when single selection, "yellow" is emitted', () => {
+  test('when single selection, "yellow" is emitted', async () => {
     wrapper.setProps({ val: undefined });
+    await wrapper.vm.$nextTick();
     wrapper.findAll('button').at(1).trigger('click');
+    await wrapper.vm.$nextTick();
     expect(wrapper.emitted('input')).toBeTruthy();
     expect(wrapper.emitted('input')).toEqual([['yellow']]);
   });
 
-  test('when multi selection, "[ yellow, blue ]" is emitted', () => {
+  test('when multi selection, "[ yellow, blue ]" is emitted', async () => {
     wrapper.findAll('button').at(1).trigger('click');
+    await wrapper.vm.$nextTick();
     wrapper.findAll('button').at(2).trigger('click');
+    await wrapper.vm.$nextTick();
     expect(wrapper.emitted('input')).toBeTruthy();
     expect(wrapper.emitted('input')).toEqual([[['yellow']], [['blue']]]);
   });
