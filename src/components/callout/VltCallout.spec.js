d0a290f3d6d579cb241252667a1d3672d6356aa9
Grreg Frileux <greg.frileux@vonage.com>
2021-02-02 10:28:18 +0000

    fix callout test

--- src/components/callout/VltCallout.spec.js
+++ src/components/callout/VltCallout.spec.js
@@ -1,30 +1,34 @@
-import { shallowMount } from '@vue/test-utils';
+import { mount } from '@vue/test-utils';
 import { VltCallout } from '..';
 
 describe('vlt-callout', () => {
   let wrapper;
 
   beforeEach(() => {
-    wrapper = shallowMount(VltCallout);
+    wrapper = mount(VltCallout);
   });
 
   test('renders correctly', () => {
     expect(wrapper.element).toMatchSnapshot();
   });
 
-  test('is type critical', () => {
+  test('is type critical', async () => {
     wrapper.setProps({ type: 'critical' });
+    await wrapper.vm.$nextTick();
     expect(wrapper.element).toMatchSnapshot();
   });
 
-  test('is dismissable', () => {
+  test('is dismissable', async () => {
     wrapper.setProps({ dismissable: true });
+    await wrapper.vm.$nextTick();
     expect(wrapper.element).toMatchSnapshot();
   });
 
   test('is dismissed', async () => {
     wrapper.setProps({ dismissable: true });
+    await wrapper.vm.$nextTick();
     wrapper.find('button').trigger('click');
+    await wrapper.vm.$nextTick();
     expect(wrapper.emitted('dismissed')).toBeTruthy();
     expect(wrapper.element).toMatchSnapshot();
   });
