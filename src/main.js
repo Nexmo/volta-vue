cc04433c5b9b4c12bdad3afba7e91416f2aff7ef
Andrea Celletti <celletti.andrea87@gmail.com>
2022-06-01 10:13:06 +0100

    Fix warning about router-link not being defined as a component.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
-import { createApp } from 'vue';
+import Vue, { createApp } from 'vue';
+import { createRouter, createMemoryHistory } from 'vue-router';
 import App from './demo/App';
 
-const app = createApp(App);
-app.mount('#app');
+// get rid of the warnings for side-navigation
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes: [{ path: '/:catchAll(.*)', component: new Vue() }],
+});
+
+createApp(App).use(router).mount('#app');
